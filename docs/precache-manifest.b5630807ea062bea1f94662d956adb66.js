0d592b7020bacd02eada4041dfc5f3e670d63b58
Mikhail Bobrutskov <wizzardobox@gmail.com>
2020-08-26 20:37:02 +0200

    added missed docs

---
+++ docs/precache-manifest.b5630807ea062bea1f94662d956adb66.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103dbc62af90f12f20687b3962c6240f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2a644af93e2bdb52b1ef.bundle.js"
+  },
+  {
+    "revision": "4a57b89213c2a6cc1d19",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.2a644af93e2bdb52b1ef.bundle.js"
+  },
+  {
+    "url": "vendors~main.2a644af93e2bdb52b1ef.bundle.js"
+  }
+]);

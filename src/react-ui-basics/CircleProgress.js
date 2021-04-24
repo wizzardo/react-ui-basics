1ca654e0ac54484bfa328af9dfaffe3cc856d842
Mikhail Bobrutskov <wizzardobox@gmail.com>
2021-04-24 18:15:51 +0200

    0.0.107 CircleProgress: minor fix for preact

--- src/react-ui-basics/CircleProgress.js
+++ src/react-ui-basics/CircleProgress.js
@@ -8,14 +8,17 @@ const length = 2 * Math.PI * radius;
 
 const dashoffset = (percent) => length * (100 - percent) / 100;
 
-const CircleProgress = ({value}) => (
-    <div className="CircleProgress">
-        <svg viewBox="0 0 24 24">
-            <path className="line" style={{strokeDashoffset: dashoffset(Number.isFinite(value) ? value : 0), strokeDasharray: length}}
-                  d="M12 3.25A8.75 8.75 0 1 1 3.25 12 A 8.75,8.75 0 0 1 12,3.25"/>
-        </svg>
-    </div>
-);
+const CircleProgress = ({value}) => {
+    value = Number.parseFloat(value);
+    return (
+        <div className="CircleProgress">
+            <svg viewBox="0 0 24 24">
+                <path className="line" style={{strokeDashoffset: dashoffset(Number.isFinite(value) ? value : 0), strokeDasharray: length}}
+                      d="M12 3.25A8.75 8.75 0 1 1 3.25 12 A 8.75,8.75 0 0 1 12,3.25"/>
+            </svg>
+        </div>
+    );
+};
 
 CircleProgress.defaultProps = {
     value: 0,

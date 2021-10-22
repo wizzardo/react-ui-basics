41879c6ac798521745bfbe7f228d6c503b94056f
Mikhail Bobrutskov <wizzardobox@gmail.com>
2021-10-22 11:00:11 +0200

    fixed warnings

--- rollup.config.js
+++ rollup.config.js
@@ -96,6 +96,7 @@ export default {
                 ["@babel/plugin-proposal-nullish-coalescing-operator", {"loose": true}],
                 "@babel/plugin-proposal-do-expressions",
 
+                ["@babel/plugin-proposal-private-methods", { "loose": true }],
                 ["@babel/plugin-proposal-private-property-in-object", {"loose": true}],
                 ["@babel/plugin-proposal-class-properties", {"loose": true}],
                 "@babel/plugin-syntax-dynamic-import",

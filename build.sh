5fe52102213b8382c3a142348557db9bbbe7e579
Mikhail Bobrutskov <wizzardobox@gmail.com>
2022-03-17 19:55:11 +0100

    0.0.147  fixing macos sed

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ NODE_ENV=publish rollup -c rollup.config.js
 cp package.json build/
 cp src/react-ui-basics/*.css build/
 
+cp build/_virtual/_rollupPluginBabelHelpers.js ./
+
 if ! md5sum -c helpers.md5; then
     echo 'helpers updated, please check the changes'
     exit 1
@@ -14,5 +16,10 @@ cp helpers.js build/_virtual/_rollupPluginBabelHelpers.js
 # md5sum build/_virtual/_rollupPluginBabelHelpers.js > helpers.md5
 
 
-sed -i "s/window.isNotProductionEnvironment/process.env.NODE_ENV !== 'production'/g" build/*.js
-sed -i "s/window.isNotProductionEnvironment/process.env.NODE_ENV !== 'production'/g" build/router/*.js
+sed -i.bak "s/window.isNotProductionEnvironment/process.env.NODE_ENV !== 'production'/g" build/*.js
+sed -i.bak "s/window.isNotProductionEnvironment/process.env.NODE_ENV !== 'production'/g" build/router/*.js
+sed -i.bak "s/window\['isNotProductionEnvironment'\]/process.env.NODE_ENV !== 'production'/g" build/*.js
+sed -i.bak "s/window\['isNotProductionEnvironment'\]/process.env.NODE_ENV !== 'production'/g" build/router/*.js
+
+rm build/*.bak
+rm build/router/*.bak

2144170a472936530aa3530de3ac303fcd4a6115
Mikhail Bobrutskov <wizzardobox@gmail.com>
2024-01-28 18:02:45 +0100

    0.0.216: Store: refactoring to Map

--- src/react-ui-basics/store/Store.ts
+++ src/react-ui-basics/store/Store.ts
@@ -2,70 +2,71 @@ import {useEffect, useState} from "react";
 import {createProxy} from "./ProxyTools";
 import {isFunction, isObject} from "../Tools";
 
-interface Stores {
-    [index: number]: any
-}
-
-let STORES: Stores = {}
-let COUNTER = 0
+const STORES = new Map<Symbol, any>()
 
 type Setter<T> = (oldState: T) => T | void;
+type ListenerFn = () => void;
+type UnsubscribeFn = () => void;
 
 abstract class AbstractStore<T> {
     get: () => T;
     set: (setter: T | Partial<T> | (Setter<T>)) => void;
     reset: () => void;
-    subscribe: (fn: () => void) => void;
-    unsubscribe: (fn: () => void) => void;
+    /**
+     * Return unsubscribe function
+     *
+     * @returns unsubscribe function.
+     */
+    subscribe: (fn: ListenerFn) => UnsubscribeFn;
     toString: () => string;
 }
+
 
 export class Store<T> extends AbstractStore<T> {
 
     constructor(initialState: T, name?: string) {
         super()
         let that = this;
-        let listeners: Array<() => void> = []
-        let index = '' + COUNTER++;
+        let listeners: Map<Symbol, ListenerFn> = new Map<Symbol, () => void>()
+        let index = Symbol();
         let runListeners = () => {
-            listeners.forEach(fn => fn())
+            for (let listener of listeners.values()) {
+                listener()
+            }
         };
 
-        STORES[index] = initialState
+        let get = (): T => STORES.get(index);
+        let reset = () => {
+            STORES.set(index, initialState)
+            runListeners();
+        };
+        reset()
 
-        that.unsubscribe = (fn: () => void) => {
-            let i = listeners.indexOf(fn)
-            let length = listeners.length;
-            if (i > -1) {
-                listeners[i] = listeners[length - 1]
-                listeners.splice(length - 1, 1)
-            }
-        }
-        that.get = (): T => STORES[index]
+        that.get = get
         that.set = (setter) => {
             let result;
             if (isFunction(setter)) {
-                const proxy = createProxy(STORES[index]);
+                const proxy = createProxy(get() as object);
                 result = (setter as Setter<T>)(proxy);
                 if (!result || result === proxy)
                     result = proxy.bake()
             } else if (isObject(setter)) {
-                result = {...STORES[index], ...setter}
+                result = {...get(), ...setter}
             } else {
                 result = setter
             }
 
-            STORES[index] = result
+            STORES.set(index, result)
             runListeners();
         }
+        that.reset = reset
 
-        that.reset = () => {
-            STORES[index] = initialState
-            runListeners();
-        }
-
-        that.subscribe = (fn: () => void) => {
-            listeners.push(fn)
+        that.subscribe = (fn: ListenerFn) => {
+            const key = Symbol();
+            listeners.set(key, fn)
+            return () => {
+                listeners.delete(key)
+            }
         }
 
         that.toString = (): string => name;
@@ -78,20 +79,12 @@ export function getGlobalState() {
     return STORES
 }
 
-export function resetGlobalState() {
-    STORES = {}
-}
-
-export function replaceGlobalState(state) {
-    STORES = state
-}
-
-
 export type Selector<T, R> = (t: T) => R;
 
 export const defaultSelector = <T>(store: T): T => store;
 
 export function useStore<T, R>(store: Store<T>, selector: Selector<T, R>, selectorDependencies: ReadonlyArray<any>): R
+export function useStore<T, R>(store: Store<T>, selector: Selector<T, R>, selectorDependencies?: undefined): R
 export function useStore<T>(store: Store<T>, selector?: undefined, selectorDependencies?: undefined): T
 export function useStore<T, R>(store: Store<T>, selector?: Selector<T, R>, selectorDependencies: ReadonlyArray<any> = []): R {
     if (!selector)
@@ -104,10 +97,7 @@ export function useStore<T, R>(store: Store<T>, selector?: Selector<T, R>, selec
             setState(selector(store.get()))
         };
         updateState()
-        store.subscribe(updateState)
-        return () => {
-            store.unsubscribe(updateState);
-        }
+        return store.subscribe(updateState)
     }, selectorDependencies)
 
     return state
